f993572e7651ba1f0e36778346fdc72662af7e44
Pollyanna79 <pollyanna79@gmail.com>
2026-05-24 19:36:53 -0300

    Atualiza URL da API para a Clever Cloud

--- react_supermercado/src/config.js
+++ react_supermercado/src/config.js
@@ -1,4 +1,4 @@
 
-const API_URL = 'https://backendsupermarket-production.up.railway.app';
+const API_URL = 'https://app-83ba2b53-841a-40af-9942-ea6b069ac54f.cleverapps.io';
 
 export default API_URL;
